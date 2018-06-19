1145600852899a8745a806849a8bf4cf16f263eb
mcauliffek4298 <kathryn@peerlift.org>
2018-06-19 18:43:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,13 @@
 #if a number is divisible by 5, return "buzz"
 #if a number is divisible by 3 and 5, return "fizzbuzz"
 def fizzbuzz(x)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  #if a number is divisible by 3, return 5
  if x%3==0
  puts "fizz"
